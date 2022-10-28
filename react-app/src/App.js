f6633e5b63a318d9bafb496b3b0f1bfa7e0c151c
Zane Erina <zaneerina@Zanes-Air.lan>
2022-10-28 18:52:28 +0300

    modified App.js file to include a class instead of function & changed content of p tag inside the main class

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,20 @@
+
+// added just for the class 
+import {Component} from 'react'; 
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// function App() {
+  class App extends Component{ //added to create a class
+  render (){ // added for class 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          Hello my name is Zane Erina
         </p>
         <a
           className="App-link"
@@ -20,6 +27,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
